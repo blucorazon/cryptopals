aca3be58058bfec6b0680ad8268cb2a8611ca7a4
blucorazon <143293307+blucorazon@users.noreply.github.com>
2024-06-14 20:27:57 -0400

    Complete Set 1 Challenge 4

---
+++ set1/detect-xor.py
@@ -0,0 +1,50 @@
+"""
+Cryptopals Challenge Set 1 Challenge 4
+
+Detect single-character XOR
+One of the 60 character strings in this file has been encrypted by
+single-character XOR. 
+
+Find it. 
+
+(Your code from #3 should help.)
+"""
+import sys
+import os
+
+# Add parent directory to system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils import hex_to_bytearray
+from utils import single_byte_xor
+from utils import scorePlaintext
+
+decoded_strings = []
+
+with open("4.txt") as file:
+    for line in file:
+        line = line.strip()
+        byteArray = hex_to_bytearray(line)
+        decoded_strings.append(byteArray)
+
+bestScore = 0
+bestKey = None
+bestPlaintext = ""
+
+for string in decoded_strings:
+    for key in range(256):
+        potential_plaintext = single_byte_xor(string, key)
+        try:
+            decoded_text = potential_plaintext.decode('utf-8')
+        except UnicodeDecodeError:
+            continue
+
+        score = scorePlaintext(decoded_text)
+
+        if score > bestScore:
+            bestScore = score
+            bestKey = key
+            bestPlaintext = decoded_text
+
+print(f"Best Key: {bestKey}")
+print(f"Message: {bestPlaintext}")
