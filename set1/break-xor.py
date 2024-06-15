c205a9440aa7caaa4b8e6f0b965cae391828e37d
blucorazon <143293307+blucorazon@users.noreply.github.com>
2024-06-15 18:07:30 -0400

    Format break-xor.py

--- set1/break-xor.py
+++ set1/break-xor.py
@@ -20,17 +20,21 @@ wokka wokka!!!
 
 is 37. Make sure your code agrees before you proceed.
 
-3. For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+3. For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, 
+and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
 
-4. The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+4. The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed 
+perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
 
 5. Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
 
-6. Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
+6. Now transpose the blocks: make a block that is the first byte of every block, 
+and a block that is the second byte of every block, and so on.
 
 7. Solve each block as if it was single-character XOR. You already have code to do this.
 
-8. For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
+8. For each block, the single-byte XOR key that produces the best looking histogram is 
+the repeating-key XOR key byte for that block. Put them together and you have the key.
 """
 import sys
 import os
@@ -48,3 +52,5 @@ str1 = 'this is a test'
 str2 = 'wokka wokka!!!'
 
 print(f"Hamming distance: {hamming_distance(str1, str2)}")
+
+KEYSIZE = 
