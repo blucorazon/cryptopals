c0b743be7a96baaaadd591698c4a1951568bfdd7
blucorazon <143293307+blucorazon@users.noreply.github.com>
2024-06-17 03:17:09 -0400

    Continue implementation of break-xor.py

--- set1/break-xor.py
+++ set1/break-xor.py
@@ -37,7 +37,7 @@ and a block that is the second byte of every block, and so on.
 the repeating-key XOR key byte for that block. Put them together and you have the key.
 """
 import base64
-from utils.utils import hamming_distance
+from utils.utils import find_key_length
 
 with open("6.txt", "r") as file:
     # Read the base64-encoded data from the file
@@ -46,4 +46,22 @@ with open("6.txt", "r") as file:
 # Decode base64 to binary and store in byte array
 byte_array = bytearray(base64.b64decode(base64_data))
 
-print(byte_array)
+# Determine the most likely Keysize candidate
+KEYSIZE = find_key_length(byte_array)
+
+print(f"The keysize with the smallest normalized edit distance is: {KEYSIZE}")
+
+# Break the ciphertext into blocks of KEYSIZE length
+for i in range(0, len(byte_array)):
+    block = byte_array[i * KEYSIZE : (i + 1) * KEYSIZE]
+
+transposed_blocks = [[] for _ in range(KEYSIZE)]
+
+# TODO: Transpose the blocks
+for byte in block:
+    transposed_blocks.append(byte)
+
+# TODO: Solve each block as if it was single byte XOR
+
+# TODO: For each block the highest scoring XOR key is the repeating-key xor key byte for that block
+# Append them as you iterate through the block to create the key
